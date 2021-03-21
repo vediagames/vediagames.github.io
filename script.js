b2dcd8f8ebefdfd94cc95be2a4ffb4c32a6e37fc
equalamount <40711615+equalamount@users.noreply.github.com>
2021-03-21 07:27:17 +0100

    +

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function placeImg(imgSrc, parent, size, x_pos, y_pos, fromLeft, fromTop, flipX) {
-    var parent = document.querySelector(`#${parent}`)
+function placeImg (imgSrc, parent, size, x_pos, y_pos, fromLeft, fromTop, flipX) {
+  var parent = document.querySelector(`#${parent}`)
 
-    let img = document.createElement("img")
-    img.src = "img/" + imgSrc
-    img.width = size;
-    img.style.position = "absolute";
+  const img = document.createElement('img')
+  img.src = 'img/' + imgSrc
+  img.width = size
+  img.style.position = 'absolute'
 
-    img.style[fromLeft ? 'left' : 'right'] = x_pos + 'px';
-    img.style[fromTop ? 'top' : 'bottom'] = y_pos + 'px';
+  img.style[fromLeft ? 'left' : 'right'] = x_pos + 'px'
+  img.style[fromTop ? 'top' : 'bottom'] = y_pos + 'px'
 
-    flipX ? img.style.transform = "scaleX(-1)" : null
+  flipX ? img.style.transform = 'scaleX(-1)' : null
 
-    parent.append(img)
+  parent.append(img)
 }
